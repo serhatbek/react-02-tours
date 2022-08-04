851c9dcbbd3c80b9a36b684310173705d7b6848f
serhatbek <serhat.bekk@gmail.com>
2022-08-04 16:09:24 +0300

    fetching tours data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import './App.scss';
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Loading from './components/Loading/Loading';
+import Tours from './components/Tours/Tours';
+
+const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-  return <div className='App'>App</div>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  console.log('tours hook', tours);
+
+  const fetchToursData = async () => {
+    setLoading(true);
+
+    try {
+      const res = await axios.get(url);
+      const data = await res.data;
+      setLoading(false);
+      setTours(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchToursData();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/TourCard/TourCard.js
@@ -0,0 +1,17 @@
+const TourCard = () => {
+  return (
+    <div className='tour-card'>
+      <figure></figure>
+      <div>
+        <h3>
+          <span></span>
+          <span></span>
+        </h3>
+        <p></p>
+        <button>Not Interested</button>
+      </div>
+    </div>
+  );
+};
+
+export default TourCard;
